574bb6cfb91ca20c9f0c3329b72405bc07d912d3
laki <giniclazar49@gmail.com>
2025-06-03 21:07:12 +0200

    Migracija/DB fix

---
+++ salalal/Migrations/20250603183754_NovaMigracija.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace salalal.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovaMigracija : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Skis",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Skis",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Skis");
+
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Skis");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SkiId1",
+                table: "OrderItems",
+                type: "int",
+                nullable: true);
+
+
+        }
+    }
+}
